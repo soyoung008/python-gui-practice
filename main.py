6a4ba09a4a2a8432f9d71af62b66b5a3cbdd82e2
soyoung <xxioo00@likelion.org>
2023-07-25 23:33:48 +0900

    Button 실습 1

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import tkinter
-
+import datetime 
 
 window=tkinter.Tk()
 
-window.title("이소영")
+window.title("What time?")
 window.geometry("640x400+100+100")
 window.resizable(False, False)
 
-label=tkinter.Label(window, text="안녕하세요.")
+label=tkinter.Label(window, text="Python", width=10, height=5, fg="red", relief="solid")
 label.pack()
 
-btn = tkinter.Button(window, text="버튼")
+def what_time():
+    print(datetime.datetime.now())
+
+what_time()
+btn=tkinter.Button(window)
+btn.config(width=10)
+btn.config(text = "현재 시각")
+btn.config(command = what_time)
+
 btn.pack()
 
 window.mainloop()
