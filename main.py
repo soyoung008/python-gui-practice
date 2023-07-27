abf9250cfd6595ddbd09cf83bfe30480e61decbb
soyoung <xxioo00@likelion.org>
2023-07-27 17:02:49 +0900

    Button 실습 2

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ window.title("What time?")
 window.geometry("640x400+100+100")
 window.resizable(False, False)
 
-label=tkinter.Label(window, text="Python", width=10, height=5, fg="red", relief="solid")
+count = 0
+
+def countUP():
+    global count
+    coutn+=1
+    label.config(text=str(count))
+    
+label = tkinter.Label(window, text="0")
 label.pack()
 
-def what_time():
-    print(datetime.datetime.now())
-
-what_time()
-btn=tkinter.Button(window)
-btn.config(width=10)
-btn.config(text = "현재 시각")
-btn.config(command = what_time)
-
-btn.pack()
+button = tkinter.Button(window, overrelife="solid", width=15, command=countUP, repeatdelay=1000, repeatinterval=100)
+button.pack()
 
 window.mainloop()
